adf7e5547ce354946cd7baf23b86a65717503e21
livia <liviaasmaia@gmail.com>
2024-03-05 06:29:01 -0300

    Atividade para filtrar estudantes da lista com lambda

--- kotlin-4-devs-java/src/main/kotlin/programacaofuncional/Lambds.kt
+++ kotlin-4-devs-java/src/main/kotlin/programacaofuncional/Lambds.kt
@@ -5,4 +5,7 @@ fun main() {
 
     val combos = students.map { a -> "${a.name} : ${a.age}" }
     println("Combos: $combos")
+
+    println("The oldest student is: ${students.maxByOrNull {  it.age }}")
+    println("Student with longest name is: ${students.filter { it.name.length > 5 }}")
 }
